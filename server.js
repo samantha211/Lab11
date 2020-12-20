ba82b8c424a0af1179f5554e22573c7dd23ffe93
samanthalloyds <samantha.c.lloyds@gmail.com>
2020-12-20 16:41:49 +0400

    initial commit

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/routing-demo'));
+app.use(express.static(__dirname + '/dist/Routing-Demo-Class7 hw'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/routing-demo/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Routing-Demo-Class7 hw/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
